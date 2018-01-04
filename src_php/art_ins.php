3dfed1f2df298e6cc31703aa21d6fa21a60575ba
xmury <xmury.ya@yandex.ru>
2018-01-04 16:50:20 +0600

    art_ins [60%]

--- src_php/art_ins.php
+++ src_php/art_ins.php
@@ -2,36 +2,101 @@
 function search($pole, $N){
     
     for ($x = 0; $x < $N; $x++){
-        $null  = 0;
-        $tire  = 0;
-        $krest = 0;
-
+        
+        $null  = 0; $krest = 0;
         for ($y = 0; $y < $N; $y++){
             switch ($pole[x][y]){
-                case '-':   $tire++ ; break;
+                case '-':   $t = [x,y] ; break;
                 case '0':   $null++ ; break;
                 case 'X':   $krest++; break;
             }   
 
-            if ($tire == 2 || $null == 2 || $krest == 2){
-                $y = 0; if ($pole[$x][$y] == '-'){ return 1, array($x,$y)}
-                $y = 1; if ($pole[$x][$y] == '-'){ return 1, array($x,$y)}
-                $y = 2; if ($pole[$x][$y] == '-'){ return 1, array($x,$y)}
+            if ($null == 2 || $krest == 2){
+                return $t;
             }
         }
 
     }
 
-    
+    for ($i = 0; $i < 2; $i++){
+        $null = 0; $krest = 0;
 
+        switch ($i) {   // Какую диагональ проверять?
+            case 0:
+                $y = 0; $x = 0;
+                break;        
+            case 1:
+                $y = 0; $x = 2;
+                break;
+        }
+
+        $q = true;
+        while ($q){
+            switch ($pole[x][y]){
+                case '-':   $t = [x,y]; break;
+                case '0':   $null++ ; break;
+                case 'X':   $krest++; break;
+            }
+
+            if ($null == 2 || $krest == 2) {
+                return $t;
+            }
+
+            switch ($i) {   // Какую диагональ проверять?
+                case 0:
+                    $y++; $x++; 
+                    if ($x > 2) { break; }
+                    break;
+                
+                case 1:
+                    $y++; $x--; 
+                    if ($x > 2) { break; }
+                    break;
+            }
+        }
+    }
+    return NULL;
 }
 
 
 function art_in_1($pole){
     $sign = '0';
 
+    $box = search();
+    if (gettype($value) == 'NULL'){
+        while (true){
+            $x = random_int(0,2);
+            $y = random_int(0, 2);
 
+            if (pole[x][y] == '-'){
+                $pole[x][y] = $sign; break;
+            }
+        }
+    }
+    else{
+        $pole[$box[0]][$box[1]] = $sign;
+    }
 
 }
 
+function art_in_2($pole)
+{
+    $sign = 'X';
+
+    $box = search();
+    if (gettype($value) == 'NULL') {
+        while (true) {
+            $x = random_int(0, 2);
+            $y = random_int(0, 2);
+
+            if (pole[x][y] == '-') {
+                $pole[x][y] = $sign;
+                break;
+            }
+        }
+    } else {
+        $pole[$box[0]][$box[1]] = $sign;
+    }
+
+}
 ?>
--- src_php/main.php
+++ src_php/main.php
@@ -1,17 +1,27 @@
 <?php
-function poligon(){
-    pole = array(array());
+function printer($pole, $N){
+    for ($x = 0; $x < $N; $x++) {
+        for ($y = 0; $y < $N; $y++) {
+            echo $pole[$x][$y], ' | ';
+        }
+        echo PHP_EOL;
+    }
+}
+function generator($N){
+    $pole = array(array());
     for ($x = 0; $x < $N; $x++){
         for ($y = 0; $y < $N; $y++){
             $pole[$x][$y] = '-'; 
         }
     }
 
-    for ($x = 0; $x < $N; $x++){
-        for ($y = 0; $y < $N; $y++){
-            echo $pole[$x][$y] , ' | '; 
-        }
-        echo '\n';
-    }
+    return $pole;
 }
+
+function poligon(){
+    $pole = generator(3);
+    printer($pole, 3);
+}
+
+poligon();
 ?>
